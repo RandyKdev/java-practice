a7753c970ef4b89922ffe243eb7318ff38a904b4
RandyKdev <rkwalar@gmail.com>
2021-11-19 05:34:54 +0100

    Add data types

---
+++ data_types.java
@@ -0,0 +1,16 @@
+class DataTypes {
+    public static void main(String[] args) {
+        // primitive types
+        int i = 2;
+        double j = 4.3;
+        char c = 'g';
+        String str = "Hey";
+        boolean bool = true;
+
+        System.out.println(i);
+        System.out.println(j);
+        System.out.println(c);
+        System.out.println(str);
+        System.out.println(bool);
+    }
+}

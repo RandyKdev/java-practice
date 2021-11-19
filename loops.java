a4e0291d474c649fba022caa24bab7c01efefc0b
Randy Kwalar <rkwalar@gmail.com>
2021-11-19 18:43:05 +0100

    Add loops

---
+++ loops.java
@@ -0,0 +1,10 @@
+public class loops {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6};
+
+        // i is the values in arr from left to right
+        for(int i:arr) {
+            System.out.println(i);
+        }
+    }    
+}

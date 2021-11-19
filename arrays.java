846eeecb6f6bcc75d4063b6e5a2ca5d1c968bb4e
Randy Kwalar <rkwalar@gmail.com>
2021-11-19 18:37:37 +0100

    Add arrays

---
+++ arrays.java
@@ -0,0 +1,18 @@
+public class arrays {
+    public static void main(String[] args) {
+        // declaring a fixed length array of integers
+        int[] arr = new int[10];
+
+        // inserting at index 0
+        arr[0] = 9;
+
+        // printing the length
+        System.out.println(arr.length);
+
+        String[] arr1 = new String[20];
+        boolean[] bool = new boolean[2];
+
+        // rule of thumb: use primitive data types combines with
+        // square brackets
+    }
+}

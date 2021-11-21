4bc38f6cacb4ac4ee39c750664184ddb002fb5f9
RandyKdev <rkwalar@gmail.com>
2021-11-21 19:28:34 +0100

    Add array list

---
+++ arrayList.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class arrayList {
+    public static void main(String[] args) {
+        // ArrayList is used to create a dynamic arrays of elements
+        ArrayList<Integer> s = new ArrayList<Integer>();
+        s.add(1); // adds an element in to array list
+        s.size(); // gets the size of the list
+        s.get(4); // gets element at index 4
+        s.remove(2); // removes element at index 2
+    }
+}

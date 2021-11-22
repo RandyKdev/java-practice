3826b0fc0421fdab72ff409223dcadb7bbb08bcb
Randy Kwalar <rkwalar@gmail.com>
2021-11-22 21:28:22 +0100

    Add class lessons

---
+++ classLesson.java
@@ -0,0 +1,19 @@
+// to define a class
+// access_modifier class name {}
+// access_modifier could be public, private, protected
+// name could be anything
+public class classLesson {
+    // classes could have attributes
+    public String name;
+    private int id;
+    protected double age;
+
+    // Static keyword = means the method or attribute could be accessed or called
+    // without necessary creating an instance of the class
+    // and also attributes which contain the static keyword are called class variables
+    // because they do not change based on the instance of the class, they are thesame for
+    // all instances
+    public static void main(String[] args) {
+        // methods can be written in classes just like this
+    }
+}
